826bdc7782c68f54c282e97d3868ea16eba6761d
MADARA X CODER <ragini.19854@gmail.com>
2026-08-06 08:47:13 +0530

    Update Youtube.py

--- MADARAMUSIC/platforms/Youtube.py
+++ MADARAMUSIC/platforms/Youtube.py
@@ -10,7 +10,7 @@ import aiohttp
 
 API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBots5ZLKEOalYeYT14ANMNip") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsfXpmXWX3cUvi7EB5MlvI") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
